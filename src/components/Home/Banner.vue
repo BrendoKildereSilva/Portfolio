8c00ec138ee3ef075dfb0b7733ad91d9d63ff667
BrendoKildereSilva <kilderebrendo8080@gmail.com>
2025-03-07 07:54:59 -0400

    starting project

---
+++ src/components/Home/Banner.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    COMPONENT: Banner
+</div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+        <Header/>
+        <Banner/>
+        <Footer/>
+</template>
+
+
+<script>
+import Header from '../components/Header.vue'
+import Banner from '../components/Home/Banner.vue'
+import Footer from '../components/footer.vue'
+
+export default {
+    components: {
+        Footer,
+        Header,
+        Banner
+    }
+}
+</script>
