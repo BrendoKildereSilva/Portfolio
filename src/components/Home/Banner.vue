800926d89faddee4de0474c6a452607de4a644f6
BrendoKildereSilva <kilderebrendo8080@gmail.com>
2025-03-10 01:42:29 -0400

    Home page

--- src/components/Home/Banner.vue
+++ src/components/Home/Banner.vue
@@ -1,20 +1,79 @@
 <template>
-    <spline-viewer id="meuSpline" url="https://prod.spline.design/mGqVdLApf6Ollvl8/scene.splinecode"></spline-viewer>
+    <div class="container-3d">
+   
+
+    <spline-viewer id="meuSpline" url="https://prod.spline.design/mGqVdLApf6Ollvl8/scene.splinecode"></spline-viewer>   
+    
+        <div class="box-shadow">
+            <button class="arrow-bottom">
+                <i class="fi fi-ts-angle-double-small-right"></i>
+            </button>
+        </div>
+    </div>
 </template>
 
 
 
 <style scoped>
+   
+
+    .container-3d{
+        height: 80vh;
+        position: relative;
+    }
+
+    .box-shadow{
+        height: 101%;
+        width: 100%;
+        position: absolute;
+        top: 0px;
+        box-shadow: inset 0px -200px 100px -100px rgba(0, 0, 0, 1);
+        pointer-events: none;
+    }
+
     #meuSpline{
-        height: 80vh;
+        width: 100%;
+        height: 100%;
+    }
+
+    .arrow-bottom{
+        height: 5rem;
+        width: 5rem;
+        background: #ffffff;
+        border-radius: 50%;
+
+        position: fixed;
+        bottom: 2rem;
+        left: 50%;
+        transform: translateX(-50%) rotate(90deg);;
+        border: 0px;
+
+    }
+
+    .arrow-bottom i{
+         display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        font-weight: bold;
+        font-size: 2rem;
+        color: #000000;
     }
 
 
-    @media(max-width: 900px){
-       #meuSpline{
-        height: 80vh;
-    } 
+    @media(max-width: 1000px){
+        .container-3d{
+            height: 90vh;
+        }
     }
+
+
+    @media(max-height:500px){
+         .box-shadow{
+            box-shadow: inset 0px -100px 50px -50px rgba(0, 0, 0, 1);
+        }
+    }
+
 </style>
 
 <script>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,91 @@
 <template>
-    <div>
-        FOOTER
-    </div>
+    <footer>
+        <span>CONTANT</span>
+        <div>
+            <a href="">Whatsapp</a>
+            <a href="">Email</a>
+            <a href="">Instagram</a>
+            <a href="">Github</a>
+            <a href="">Linkedin</a>
+        </div>
+
+        <p>Design and development by Brendo Kildere ©.</p>
+    </footer>
 </template>
+
+
+<style scoped>
+
+    footer{
+        z-index: 2;
+        height: 50vh;
+        background: #000;
+
+        width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+
+    span{
+        margin-top: 2rem;
+        font-size: 4rem;
+        color: #fff;
+    }
+
+    div{
+        width: 100%;
+        margin: 2rem;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+
+    a{
+        position: relative;
+        font-size: 1.5rem;
+        color: #fff;
+        margin: 2rem;
+        text-decoration: none;
+    }
+
+    a:hover{
+        /* font-size: 2rem; */
+        text-decoration: underline;
+    }
+
+    a::after{
+        content: "";
+        display: inline-block;
+        height: 2rem;
+        width: 0.2rem;
+        top: 0rem;
+        right: -2.5rem;
+        background: rgb(255, 255, 255);
+        position: absolute;
+    }
+
+    a:nth-child(5)::after{
+        display: none;
+    }
+
+    p{
+        font-size: 1.5rem;
+        color: #fff;
+        text-align: center;
+    }
+
+    @media(max-height: 500px){
+        footer{
+            height: 80vh;
+        }
+    }
+    @media(max-width: 390px){
+        a:nth-child(5)::after{
+            display: none;
+        }
+    }
+
+</style>
